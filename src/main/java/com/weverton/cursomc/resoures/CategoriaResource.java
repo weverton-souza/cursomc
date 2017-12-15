d163460218eeece64ac9a07cdc57b66672dfefc5
Weverton da Silva <wevertonad@gmail.com>
2017-12-14 23:43:02 -0200

    Testando Rest

---
+++ src/main/java/com/weverton/cursomc/resoures/CategoriaResource.java
@@ -0,0 +1,15 @@
+package com.weverton.cursomc.resoures;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value="/categorias")
+public class CategoriaResource {
+	
+	@RequestMapping(method=RequestMethod.GET)
+	public String list() {
+		return "Rest funcionando!";
+	}
+}
